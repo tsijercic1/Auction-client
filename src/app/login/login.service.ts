5c4e474065ea9dc38f457eea537eb7438655338b
Tarik Sijerčić <tsijercic1@etf.unsa.ba>
2020-02-16 20:59:21 +0100

    Added login service and implemented post login

---
+++ src/app/login/login.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface LoginResponseData {
+  accessToken: string;
+  tokenType: string;
+}
+
+@Injectable({providedIn: 'root'})
+export class LoginService {
+  constructor(private http: HttpClient) {
+
+  }
+  login(email: string, password: string) {
+    return this.http.post<LoginResponseData>(
+      'http://localhost:8080/api/auth/login',
+      {
+        emailOrUsername: email,
+        password
+      });
+  }
+}
